2edbd0bf952fae58bc43ef6b3da078dc2f2fd8d9
Olik-Lelik <rbvolga14@gmail.com>
2022-07-24 04:08:06 +0300

    Правда или действие (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,25 +1,21 @@
 import { openPictureModal, closePictureModal } from './picture-modal.js';
 
+const LIMIT = 5;
+
 const closeButton = document.querySelector('#picture-cancel');
 const bigPictureImg = document.querySelector('.big-picture__img img');
 const bigPictureImgCaption = document.querySelector('.social__caption');
 const bigPictureImgLikes = document.querySelector('.likes-count');
-const commentCount = document.querySelector('.comments-count');
+const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsList = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment').cloneNode(true);
-const sectionBigPicture = document.querySelector('.big-picture');
+const commentsLoaderButton = document.querySelector('.comments-loader');
 
-
-const pictureModalCloseElement = (elem) => {
-  closeButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    closePictureModal(elem);
-  });
-};
+let startIndex = 0;
+let value = 0;
+let photoComments;
 
 const createCommentItem = (comments) => {
-  commentCount.textContent = comments.length;
-
   comments.forEach(({avatar, name, message}) => {
     const commentItem = socialComment.cloneNode(true);
 
@@ -31,22 +27,69 @@ const createCommentItem = (comments) => {
   });
 };
 
-const creatFillPicture = ({url, description, comments, likes}) => {
+const createLoadComments = () => {
+  startIndex = value * LIMIT;
+  const endIndex = startIndex + LIMIT;
+
+  const commentsVisible = photoComments.slice(startIndex, endIndex);
+
+  createCommentItem(commentsVisible);
+  socialCommentCount.textContent = `показанo ${commentsVisible.length} из ${photoComments.length} комментариев`;
+
+  if (endIndex > 5 && endIndex < photoComments.length) {
+    socialCommentCount.textContent = `показанo ${endIndex} из ${photoComments.length} комментариев`;
+  } else
+  if (endIndex >= photoComments.length) {
+    commentsLoaderButton.classList.add('hidden');
+    socialCommentCount.textContent = 'всё';
+  }
+
+  value++;
+};
+
+const createFillPicture = ({url, description, comments, likes}) => {
   bigPictureImg.src = url;
   bigPictureImg.alt = description;
   bigPictureImgCaption.textContent = description;
   bigPictureImgLikes.textContent = likes;
+  photoComments = comments;
+
+  commentsLoaderButton.classList.add('hidden');
 
   commentsList.innerHTML = '';
-  createCommentItem(comments);
+
+  if (comments.length > 5) {
+    commentsLoaderButton.classList.remove('hidden');
+    createLoadComments();
+    commentsLoaderButton.addEventListener('click', createLoadComments);
+  } else {
+    if (comments.length === 1) {
+      socialCommentCount.textContent = `показан ${comments.length} комментарий`;
+    } else
+    if (comments.length > 1 && comments.length < 5) {
+      socialCommentCount.textContent = `показано ${comments.length} комментария`;
+    } else
+    if (comments.length === 5) {
+      socialCommentCount.textContent = `показанo ${comments.length} комментариев`;
+    }
+    createCommentItem(comments);
+  }
 };
+
+const pictureModalCloseElement = () => {
+  closeButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    closePictureModal();
+  });
+};
+
 
 const showBigPicture = ({url, description, comments, likes}) => {
-  openPictureModal(sectionBigPicture);
+  openPictureModal();
+  value = 0;
+  createFillPicture({url, description, comments, likes});
 
-  creatFillPicture({url, description, comments, likes});
-
-  pictureModalCloseElement(sectionBigPicture);
+  pictureModalCloseElement();
 };
 
-export {showBigPicture};
+export {showBigPicture, commentsLoaderButton, createLoadComments};
